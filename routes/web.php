0847efeb04a213160e84021b4431c2f99568adac
Abim Must <abimmustawa12@gmail.com>
2026-02-16 23:57:34 +0700

    Jobsheet2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +20,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Selamat Datang');
 });
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+
+Route::get ('/world', function (){
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return ('244107020078 | Abim Mustawa');
+});
+
+Route::get('/user/{name}', function ($name) {
+    return ('Nama Saya '.$name);
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return ('Halaman artikel dengan ID '.$id);
+});
+
+
+Route::get('/user/{name?}', function ($name='John') {
+    return ('Nama Saya '.$name);
+});
+
